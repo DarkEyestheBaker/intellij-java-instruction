2bb6e80cae8f8f86d466d4d38c105d9267610516
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-06-03 18:21:34 -0400

    Primitive vs Reference

---
+++ src/com/darkeyesthebaker/primative_reference.java
@@ -0,0 +1,12 @@
+package com.darkeyesthebaker;
+
+import java.awt.*;
+
+public class primative_reference {
+    public static void main(String[] args) {
+        Point point1 = new Point(x:1, y:1);
+   System.out.println(y);
+    }
+}
+
+
